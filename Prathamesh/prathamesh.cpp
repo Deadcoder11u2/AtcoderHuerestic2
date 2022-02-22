4ab9e12e0ebd62bfba96af0ed55b667edbea6c29
Prathamesh <2002pratham1109@gmail.com>
2022-02-22 13:21:39 +0530

    Score function updated.. working

--- Prathamesh/prathamesh.cpp
+++ Prathamesh/prathamesh.cpp
@@ -131,51 +131,131 @@ void debug_code()
 #endif
 }
 
-class maze
+class Maze
 {
 public:
     int n;
-    vector<vector<int>> pos;
-    vector<vector<int>> val;
+    pair<int,int>xy;
+    string s;
+    vector<vector<pair<int, int>>> matrix;
 
-    maze(){
-        n =  50;
+    Maze()
+    {
+        n = 50;
+        int sx, sy;
+        cin >> sx >> sy;
+        xy.first = sx, xy.second = sy;
+
+        matrix.resize(50, vector<pair<int, int>>(50, {0, 0}));
     }
 
-    void readpos()
+    void Read()
     {
+        vector<vector<int>> val;
+        vector<vector<int>> pos;
+
         for (int i = 0; i < n; i++)
         {
             vector<int> temp(50);
             for (int j = 0; j < n; j++)
-            {
                 cin >> temp[j];
-            }
             pos.push_back(temp);
         }
-    }
-    void readval()
-    {
+
         for (int i = 0; i < n; i++)
         {
             vector<int> temp(50);
             for (int j = 0; j < n; j++)
-            {
                 cin >> temp[j];
-            }
             val.push_back(temp);
         }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j].first = pos[i][j];
+                matrix[i][j].second = val[i][j];
+            }
+        }
+
     }
 };
+
+int score(pair<int, int> current, vector<vector<pair<int, int>>> &matrix, string s)
+{
+
+    int finalScore = 0;
+    pair<int, int> xy = current;
+
+    vector<int>scores;
+    vector<int>positions;
+
+    vector<vector<bool>> Istrue(50, vector<bool>(50, true));
+
+    for (int i = 0; i <= size(s); i++){
+
+        if (Istrue[xy.first][xy.second] == false)
+            return -1;
+        else{
+            Istrue[xy.first][xy.second] = false;
+
+            if(xy.first - 1 >= 0 && matrix[xy.first - 1][xy.second].first == matrix[xy.first][xy.second].first)
+                Istrue[xy.first - 1][xy.second] = false;
+
+            else if(xy.second - 1 >= 0 && matrix[xy.first][xy.second - 1].first == matrix[xy.first][xy.second ].first)
+                Istrue[xy.first][xy.second - 1] = false;
+
+            else if(xy.first + 1 <= 49 && matrix[xy.first + 1][xy.second].first == matrix[xy.first][xy.second].first)
+                Istrue[xy.first + 1][xy.second] = false;
+
+            else if(xy.second + 1 <=  49 && matrix[xy.first][xy.second + 1].first == matrix[xy.first][xy.second].first)
+                Istrue[xy.first][xy.second + 1] = false;
+        }
+
+
+
+        finalScore += matrix[xy.first][xy.second].second;
+        scores.push_back(matrix[xy.first][xy.second].second);
+        positions.push_back(matrix[xy.first][xy.second].first);
+
+        if(s[i] == 'U'){
+            if(xy.first - 1 >= 0 && Istrue[xy.first - 1][xy.second] == true) xy.first = xy.first - 1;
+            else return -1;
+        }
+        else if(s[i] == 'D'){
+            if(xy.first + 1 <= 49 && Istrue[xy.first + 1][xy.second] == true) xy.first = xy.first + 1;
+            else return -1;
+        }
+        else if(s[i] == 'R'){
+            if(xy.second + 1 <= 49 && Istrue[xy.first][xy.second + 1] == true) xy.second = xy.second + 1;
+            else return -1;
+        }
+        else{
+            if(xy.second - 1 >= 0 && Istrue[xy.first][xy.second - 1] == true) xy.second = xy.second - 1;
+            else return -1;
+        }
+    }
+    debug(scores);
+    debug(positions);
+    return finalScore;
+}
 
 signed main(int argc, char **argv)
 {
     fastio();
-    // init_code();
-    // debug_code();
+    init_code();
+    debug_code();
 
-    int sx, sy;
-    cin >> sx >> sy;
+    Maze *maze = new Maze();
+    maze->Read();
 
+    // maze->s = "LDDDDDLLULLLUUULLLDDRDDLDDLDDDDDLLLDDLDDRDRDDRRRUURRURUUULULDDDRRRRRRRR";
+    // maze->s = "R";
+    // maze->s = "LU";
+
+    cout << score(maze->xy, maze->matrix, maze->s) << nline;
+
+    
     return 0;
 }
