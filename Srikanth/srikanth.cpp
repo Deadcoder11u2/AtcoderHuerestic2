80590bbc359826765a4a7e9f13613293123a6784
Srikanth Iyengar <ksrikanth3012@gmail.com>
2022-02-22 12:50:58 +0530

    added files

--- Srikanth/srikanth.cpp
+++ Srikanth/srikanth.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
-#include <score.cpp>
+#include "score.cpp"
 using namespace std;
 
-int main() {
+int n = 50;
+
+void solve() {
+    int si, sj;
+    cin >> si >> sj;
     
+    int mat[50][50];
+    for(int i = 0 ;i < n ; i++) {
+        for(int j = 0 ; j < n ; j++) cin >> mat[i][j];
+    }
+    
+    int sc[50][50];
+    for(int i = 0 ; i < n ; i++)
+        for(int j = 0 ; j < n ; j++) cin >> sc[i][j];
+    
+    
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    solve();    
 }
